4e36a4b5496a5d621a5616627c3333038888f4ff
Greshch <greshchook@gmail.com>
2021-12-02 22:34:40 +0200

    book

---
+++ cpp_pro/OOP_C_PLUS/OOP_C_PLUS/Book.h
@@ -0,0 +1,31 @@
+#pragma once
+class Book
+{
+private:
+	char* title;
+	char* author;
+	char* ganre;
+	int pages;
+	double price;
+
+public:
+	Book();
+	Book(char const* title_, char const* author_,
+		char const* ganre_, double price_, int pages_);
+	char const* GetTitle() const;
+	char const* GetAuthor() const;
+	char const* GetGanre() const;
+	int  GetPages(char const*);
+	void SetTitle(char const*);
+	void SetAuthor(char const*);
+	void SetGanre(char const*);
+	void SetPages(int);
+	void SetPrice(double dollars);
+	void Print() const;
+	~Book();
+
+private:
+	int const LEN = 80;
+	void Init();
+};
+
--- cpp_pro/OOP_C_PLUS/OOP_C_PLUS/main.cpp
+++ cpp_pro/OOP_C_PLUS/OOP_C_PLUS/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include "Stack.h"
+#include "Book.h"
 
 using namespace std;
 
 int main()
 {
-	Stack stack;
+	//////////////////////////////////////////
+	/*Stack stack;
 	for (size_t i = 0; i < 5; i++)
 	{
 		stack.Push(i + 1);
@@ -14,6 +16,14 @@ int main()
 	while (!stack.IsEmpty())
 	{
 		cout << stack.Pop() << endl;
-	}
+	}*/
+	//////////////////////////////////////////
+	
+	//////////////////////////////////////////
+	Book some_book("title", "author", "ganre", 100, 300);
+	some_book.Print();
+	//////////////////////////////////////////
+
+
 	return 0;
 }

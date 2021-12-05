af4dcceae46f267e4dc3b4641be1c40066cf7bc4
Greshch <greshchook@gmail.com>
2021-12-05 19:00:09 +0200

    point

---
+++ cpp_pro/OOP_C_PLUS/OOP_C_PLUS/Point.cpp
@@ -0,0 +1,43 @@
+#include "Point.h"
+
+Point::Point() : Point(0, 0)
+{
+}
+
+Point::Point(int x, int y)
+{
+    SetX(x);
+    SetY(y);
+}
+
+Point& Point::SetX(int x)
+{
+    _x = x;
+    return *this;
+}
+
+Point& Point::SetY(int y)
+{
+    _y = y;
+    return *this;
+}
+
+int Point::GetX() const
+{
+    return _x;
+}
+
+int Point::GetY() const
+{
+    return _y;
+}
+
+Point& Point::MoveToX(int x)
+{
+    _x += x;
+}
+
+Point& Point::MoveToY(int y)
+{
+    _y += y;
+}
---
+++ cpp_pro/OOP_C_PLUS/OOP_C_PLUS/Point.h
@@ -0,0 +1,18 @@
+#pragma once
+class Point
+{
+private:
+	int _x;
+	int _y;
+
+public:
+	Point();
+	Point(int x, int y);
+	Point& SetX(int x);
+	Point& SetY(int y);
+	int GetX() const;
+	int GetY() const;
+	Point& MoveToX(int x);
+	Point& MoveToY(int y);
+};
+
